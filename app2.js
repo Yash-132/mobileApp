1467148704b020fa10b69e646feaf0502581faf0
dev global <skooncode@gmail.com>
2025-05-04 16:12:17 +0530

    file added

---
+++ app2.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+  <h2> JavaScript Main Page</h2>
+
+  <p id="main"> JavaScript changes</p>
+  <button type="botton" onClick='document.getElementById("main").innerHTML = "Hye user Welcome!" '> Submit </button>
+</body>
+</html>
